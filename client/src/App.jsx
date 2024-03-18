68a81536946c2a53862dd7d289a1a6b09212898c
tejasnasre <tejasnasre120@gmail.com>
2024-03-18 23:04:32 +0530

    Added NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from 'react'
+import Navbar from './pages/Navbar'
+import { Outlet } from 'react-router-dom'
 
-export default App;
+export default function App() {
+  return (
+    <>
+    <Navbar />
+    <Outlet />
+    {/* <Footer/> */}
+    </>
+  )
+}
---
+++ client/src/pages/Navbar.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <nav className="bg-white dark:bg-gray-900  w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link
+            to='/'
+            className="flex flex-row items-center justify-center space-x-3 rtl:space-x-reverse"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="h-6 w-6"
+            >
+              <rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect>
+              <line x1="16" x2="16" y1="2" y2="6"></line>
+              <line x1="8" x2="8" y1="2" y2="6"></line>
+              <line x1="3" x2="21" y1="10" y2="10"></line>
+              <path d="m9 16 2 2 4-4"></path>
+            </svg>
+            <span className="text-2xl font-bold tracking-tighter sm:text-3xl">
+              Event Platform
+            </span>
+          </Link>
+          <div className="flex flex-row items-center justify-center gap-1 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button
+              onClick={toggleMenu}
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className={`items-center justify-between  w-full md:flex md:w-auto md:order-1 ${
+              isOpen ? "block" : "hidden"}`}
+            id="navbar-sticky"
+          >
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li className="flex flex-row items-center justify-center">
+                <Link
+                  to='/'
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 ml-2"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="flex flex-row items-center justify-center">
+                <Link
+                  to='/'
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ml-2"
+                >
+                  Experience
+                </Link>
+              </li>
+              <li className="flex flex-row items-center justify-center">
+                <Link
+                  to='/'
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ml-1"
+                >
+                  Weedings
+                </Link>
+              </li>
+              <li className="flex flex-row items-center justify-center">
+                <Link
+                  to='/'
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ml-2"
+                >
+                  Contact Us
+                </Link>
+              </li>
+              <li className="flex flex-row items-center justify-center">
+                <Link
+                  tp="/"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ml-1"
+                >
+                  Explore More
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
